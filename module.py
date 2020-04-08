444708d25e111a0abe52c1c0f42d64403c62c61c
Andrew Cole <andrewcole.817@gmail.com>
2020-04-08 13:20:36 -0500

    FConsolidate code in module

--- module.py
+++ module.py
@@ -7,7 +7,7 @@ def format_full(df, code_df):
     
     df['code'] = None
     df.columns = ['date', 'country', 'state', 'lat', 'long', 'confirmed', 'recovered', 'deaths', 'code']
-    df.drop(['state', 'lat', 'long'], axis = 1, inplace = True)
+    df.drop(['lat', 'long'], axis = 1, inplace = True)
     
     codes = code_df.drop(['Alpha-2 code', 'Numeric code', 'Latitude (average)', 'Longitude (average)'], axis = 1)
     codes.columns = ['country', 'code']
@@ -25,9 +25,11 @@ def format_full(df, code_df):
     df = df.sort_values(['country', 'date'])
     df.set_index('date', inplace = True)
     
-    df = df[['country', 'code', 'confirmed', 'recovered', 'deaths']]
+    df = df[['country', 'code', 'state', 'confirmed', 'recovered', 'deaths']]
     
-    
+    df = df.groupby(['country', 'date', 'code']).sum().reset_index()
+    df = df.set_index('date')
+
     
     return df
 
@@ -38,7 +40,9 @@ def create_country_dict(df):
         country_df_names.append(df_name)
         
     country_df_dict = {}
+    
     for name in country_df_names:
+     
         country_df_dict[name] = df[df.country == name]
     
     return country_df_dict
@@ -46,13 +50,13 @@ def create_country_dict(df):
 
 def plot_time_series(df_dict):
     for key in df_dict.keys():
+        
         t = df_dict[key].plot(figsize = (30, 10), color = ['blue', 'green', 'red'], lw = 2)
         t.set_xlabel('Date', fontweight = 'bold', fontsize = 25)
         t.set_ylabel('Number of Cases', fontweight = 'bold', fontsize = 25)
         t.set_title(f'{key} COVID-19 Progression', fontsize = 35, fontweight = 'bold')
         t.tick_params(axis = 'both', which = 'both', labelsize = 20)
-        t.legend(loc = 'best', prop = {'size': 20}, frameon = True, edgecolor = 'black', fancybox = True)
-        plt.tight_layout()
+        t.legend(loc = 'best', prop = {'size': 20}, frameon = True, edgecolor = 'black', fancybox = True, facecolor = 'white', framealpha = .9)
         
     return t
         
